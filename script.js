7b7fa1d6285c2ca9feff00931a35aef3b5bd0e3b
desta mulu <destish21@yahoo.com>
2020-11-25 21:37:10 -0600

    script.js

--- script.js
+++ script.js
@@ -3,59 +3,109 @@ var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
-var todos = [];
-
-init();
-
+var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+// var todos = JSON.parse(localStorage.getItem('todos')) 
+// console.log(todos)
+// init();
 function renderTodos() {
   // Clear todoList element and update todoCountSpan
   todoList.innerHTML = "";
-  todoCountSpan.textContent = todos.length;
+  todoCountSpan.textContent = 'todos.length';
 
-  // Render a new li for each todo
-  for (var i = 0; i < todos.length; i++) {
+  for (var i = 0; i < todos.length; i++ ) {
     var todo = todos[i];
-
+// create li element
     var li = document.createElement("li");
-    li.textContent = todo;
-    li.setAttribute("data-index", i);
-
-    var button = document.createElement("button");
+    // add todo text to li
+    li.innerText = todos[i]
+    // add data -index attribute
+    li.setAttribute('data-index', i);
+    // crear button
+  var button = document.createElement("button");
     button.textContent = "Complete";
-
-    li.appendChild(button);
+    li.appendChild(button)
+    // appeend li todolist
     todoList.appendChild(li);
   }
 }
+  
+renderTodos() 
+  
+  function addTodo(event) {
+    event.preventDefault()
+    // get todoinput value
+    var newTodo = todoInput.Value
+    if(newTodo !== '') {
+      // push value into todos
+      todos.push(newTodo)
+      todoInput.Value = null
+      localStorage.setItem('todos', JSON.stringify(todos))
+      renderTodos() 
 
-function init() {
-  // Write code here to check if there are todos in localStorage
-  // If so, parse the value from localStorage and assign it to the todos variable
+    }
+  }
+  
+  function removeTodo(event) {
+    var target = event.target
+    if (target.matches('button')) {
+      // find index from LI
+      var index = parseInt(target.parentNode.getAttribute('data-index'))
+      // remove the todos from todos array
+      todos.splice(index, 1)
+      // re-render todos
+      renderTodos()
+      localStorage.setItem('todos', JSON.stringify(todos))
 
-  // Render todos to the DOM
-  renderTodos();
-}
-
-function storeTodos() {
-  // Add code here to stringify the todos array and save it to the "todos" key in localStorage
-}
-
-// When form is submitted...
-todoForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  var todoText = todoInput.value.trim();
-
-  // Return from function early if submitted todoText is blank
-  if (todoText === "") {
-    return;
+    }
   }
 
-  // Add new todoText to todos array, clear the input
-  todos.push(todoText);
-  todoInput.value = "";
+  todoForm.addEventListener('submit', addTodo)
+  todoList.addEventListener('click', removeTodo)
+  renderTodos() 
 
-  // Store updated todos in localStorage, re-render the list
-  storeTodos();
-  renderTodos();
-});
+  
+  
+  
+  
+  
+
+
+  // Render a new li for each todo
+
+
+
+//     todoList.appendChild(li);
+//   }
+// }
+
+// function init() {
+//   // Write code here to check if there are todos in localStorage
+//   // If so, parse the value from localStorage and assign it to the todos variable
+
+//   // Render todos to the DOM
+//   renderTodos();
+// }
+
+// function storeTodos() {
+//   // Add code here to stringify the todos array and save it to the "todos" key in localStorage
+// }
+
+// // When form is submitted...
+// todoForm.addEventListener("submit", function(event) {
+//   event.preventDefault();
+
+//   var todoText = todoInput.value.trim();
+
+//   // Return from function early if submitted todoText is blank
+//   if (todoText === "") {
+//     return;
+//   }
+
+//   // Add new todoText to todos array, clear the input
+//   todos.push(todoText);
+//   todoInput.value = "";
+
+//   // Store updated todos in localStorage, re-render the list
+//   storeTodos();
+//   renderTodos();
+// });
